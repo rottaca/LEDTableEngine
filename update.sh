3a0645a5d84c03b2eb620532402f721857dccb82
rottaca <andirottach@aol.com>
2018-03-09 12:31:44 +0100

    Stash local chagnes before updating.

--- update.sh
+++ update.sh
@@ -5,8 +5,12 @@ exec 2>&1
 dir=$(dirname "$(readlink -f "$0")")
 echo "Go to source dir: $dir"
 cd $dir
+echo "Stash changes"
+git stash
 echo "Pull project"
 git pull
+echo "Reapply stashed changes"
+git stash apply
 echo "Goto build directory"
 cd build
 echo "CMAKE..."
