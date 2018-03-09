dec20d4662f1437a02a9e4e8f7778f13a7249815
rottaca <andirottach@aol.com>
2018-03-09 12:41:37 +0100

    Fixed update script

--- update.sh
+++ update.sh
@@ -2,18 +2,35 @@
 set -e	# Exit on error
 exec 2>&1
 
+# unique timestamp
+t=timestamp-$(date +%s)
+
 dir=$(dirname "$(readlink -f "$0")")
 echo "Go to source dir: $dir"
 cd $dir
+
 echo "Stash changes"
-git stash
+# stash with message
+r=$(git stash save $t)
+# check if the value exists
+v=$(echo $r|grep $t)
+if [ "$v" ]; then
+  echo "Nothing to stash"
+fi
+
 echo "Pull project"
-git pull
-echo "Reapply stashed changes"
-git stash apply
+git pull --no-commit
+
+if [ "$v" ]; then
+  echo "Reapply stashed changes"
+  git stash apply
+fi
+
 echo "Goto build directory"
 cd build
+
 echo "CMAKE..."
 cmake ..
+
 echo "Build project..."
 make
