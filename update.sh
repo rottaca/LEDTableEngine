9117550aa4113d4b6a56f065f01a2f90e5a73e12
rottaca <andirottach@aol.com>
2018-03-09 12:48:28 +0100

    Improved update script.

--- update.sh
+++ update.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-set -e	# Exit on error
+#set -e	# Exit on error
 exec 2>&1
 
 # unique timestamp
 t=timestamp-$(date +%s)
 
 dir=$(dirname "$(readlink -f "$0")")
-echo "Go to source dir: $dir"
+echo ">>>>>>>> Go to source dir: $dir"
 cd $dir
 
-echo "Stash changes"
+echo ">>>>>>>> Stash changes"
 # stash with message
 r=$(git stash save $t)
 # check if the value exists
-v=$(echo $r|grep $t)
+v=$(echo $r | grep $t)
 
-echo "Pull project"
+echo ">>>>>>>> Pull project"
 git pull --no-commit
 
 if [ "$v" ]; then
-  echo "Reapply stashed changes"
+  echo ">>>>>>>> Reapply stashed changes"
   git stash apply
 fi
 
-echo "Goto build directory"
+echo ">>>>>>>> Goto build directory"
 cd build
 
-echo "CMAKE..."
+echo ">>>>>>>> CMAKE..."
 cmake ..
 
-echo "Build project..."
+echo ">>>>>>>> Build project..."
 make
